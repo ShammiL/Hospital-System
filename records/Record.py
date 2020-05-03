d81d8295f00ed2a2f5fb9cabd1b8b53de410dc5b
ShammiL <47134017+ShammiL@users.noreply.github.com>
2020-05-03 21:54:41 +0530

    Add files via upload

---
+++ records/Record.py
@@ -0,0 +1,13 @@
+
+
+class Record:
+
+    def __init__(self, name, age, nic, sickness, drug_prescription):
+        self.name = name
+        self.age = age
+        self.nic = nic
+        self.sickness = sickness
+        self.drug_prescription = drug_prescription
+        self.lab_test = "No lab prescriptions recorded"
+
+
